b21e95ea8eb0e4a2aa51caea226768bdae3c4709
Eliseo Murillo <eliseomurillo9@gmail.com>
2023-07-16 23:38:18 +0200

    feat/i18n installed

---
+++ src/locales.js
@@ -0,0 +1,32 @@
+const messages = {
+    'es-ES': {
+        base: {
+            pageTitle: 'Boda Georgi & Eli',
+            weddingDate: {
+                year: '2024', month: 'Julio', day: '20'
+              },
+            counter:{
+                days: 'Dias', hours: 'Hr', minutes: 'Min', seconds: 'Seg',
+            },
+            wait: 'Espera un poco',
+            workInProgress: 'Estamos trabajando en algo especial',
+            newsLetter: 'Notificame cuando este listo',
+        }
+    },
+    'en-EN': {
+        base: {
+            pageTitle: 'Georgi & Eli Wedding',
+            weddingDate: {
+                year: '2024', month: 'July', day: '20'
+              },
+            counter:{
+                days: 'Days', hours: 'Hr', minutes: 'Min', seconds: 'Sec',
+            },
+            wait: 'Wait for it',
+            workInProgress: 'We are working in something special',
+            newsLetter: 'Notify me when it\\’s ready',
+        }
+    }
+}
+
+export default messages;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createI18n, useI18n } from "vue-i18n";
+import messages from "./locales";
+import "./index.css";
+const i18n = createI18n({
+  legacy: false,
+  locale: "en", // set locale
+  fallbackLocale: "es",
+  messages,
+});
 
-createApp(App).mount('#app')
+const app = createApp(App, i18n, {
+  setup() {
+    const { t } = useI18n(); // call `useI18n`, and spread `t` from  `useI18n` returning
+    return { t }; // return render context that included `t`
+  },
+});
+app.mount("#app");

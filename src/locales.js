4d382de3e44861447589ce558ba8b28dd82344bb
Eliseo Murillo <eliseomurillo9@gmail.com>
2023-08-05 11:09:37 +0200

    title fixed and responsive details added

--- src/locales.js
+++ src/locales.js
@@ -1,21 +1,23 @@
 const messages = {
   ES: {
     base: {
-      pageTitle: "Boda Georgi & Eli",
-      weddingDate: "21 de Julio, 2024",
+      weddingTitle: "Boda",
+      pageTitle: "{wedding} Georgi & Eli",
+      weddingDate: "20 de Julio, 2024",
       counter: {
-        days: "Dias",
+        days: "Días",
         hours: "Hr",
         minutes: "Min",
         seconds: "Seg",
       },
       wait: "Espera un poco",
       workInProgress: "Estamos trabajando en algo especial",
-      newsLetter: "Notificame cuando este listo",
+      newsLetter: "Notifícame cuando este listo",
       newletterConfirmation: "Email registered successfully",
       error: {
         noAvailable: "El servicio no esta disponible.",
         emailRegisted: "Tu correo ha esta registrado",
+        notValidEmail: "E-mail no valido",
       },
       pageTitles: {
         saveTheDate: "Save the date",
@@ -25,8 +27,9 @@ const messages = {
   },
   EN: {
     base: {
-      pageTitle: "Georgi & Eli Wedding",
-      weddingDate: "July 21th, 2024",
+      weddingTitle: "Wedding",
+      pageTitle: "Georgi & Eli {wedding}",
+      weddingDate: "July 20th, 2024",
       counter: {
         days: "Days",
         hours: "Hr",
@@ -40,10 +43,11 @@ const messages = {
       error: {
         noAvailable: "The service is not available.",
         emailRegisted: "Your e-mail is already register",
+        notValidEmail: "Not valid e-mail",
       },
       pageTitles: {
         saveTheDate: "Save the date",
-        confirmation: "Confirmation"
+        confirmation: "Confirmation",
       },
     },
   },

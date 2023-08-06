3d8fa46c74ee200c859d97700109a702ab342117
Eliseo Murillo <eliseomurillo9@gmail.com>
2023-08-06 17:56:05 +0200

    add image size in header logo

--- src/components/HeaderContainer.vue
+++ src/components/HeaderContainer.vue
@@ -3,7 +3,7 @@
   <select name="language" id="language" class="bg-inherit rounded-lg" v-model="$i18n.locale">
     <option class="text-blue-main rounded-lg" v-for="(lang,i) in languages" :key="`Lang${i}`" :value="lang">{{ lang }}</option>
   </select>
-<router-link to="/"><img :src="require('@/assets/logo.svg')" :alt="$t('imagesAlt.logo')"></router-link>
+<router-link to="/"><img :src="require('@/assets/logo.svg')" :alt="$t('imagesAlt.logo')" class="h-11 w-11"></router-link>
 </nav>
 </template>
 

2f6013f565d600eecc29ed9ac132223bba555556
Eliseo Murillo <95434225+eliseomurillo9@users.noreply.github.com>
2023-08-08 21:18:03 +0200

    Add serverless functions (#10)
    * serverless functions added
    
    * serverless functions added

---
+++ invitation-wedding-functions/getEmails/getEmails.js
@@ -0,0 +1,23 @@
+// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+require("dotenv").config();
+
+const url = process.env.SHEET_API_URL;
+const token = process.env.SHEET_API_KEY;
+
+const getEmails = async () => {
+  try {
+    const response = await fetch(url, {
+      headers: {
+        Authorization: token,
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+module.exports = { getEmails };
